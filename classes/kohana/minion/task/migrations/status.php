73dc27feca1242607f111fd50671793ed4cade86
Evan Purkhiser <evanpurkhiser@gmail.com>
2012-05-25 21:34:33 -0400

    Take advantage of the Kohana cascadinf file system
    Take advantage of the CFS by adding base clases that extend the real
    classes. This is the same technique used in the Kohana core framework.

---
+++ classes/kohana/minion/task/migrations/status.php
@@ -0,0 +1,28 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Displays the current status of migrations in all groups
+ *
+ * This task takes no config options
+ *
+ * @author Matt Button <matthew@sigswitch.com>
+ */
+class Kohana_Minion_Task_Migrations_Status extends Minion_Task {
+
+	/**
+	 * Execute the task
+	 *
+	 * @param array Config for the task
+	 */
+	public function execute(array $config)
+	{
+		$db        = Database::instance();
+		$model     = new Model_Minion_Migration($db);
+
+		$view = new View('minion/task/migrations/status');
+
+		$view->groups = $model->get_group_statuses();
+
+		echo $view;
+	}
+}
--- classes/minion/task/migrations/status.php
+++ classes/minion/task/migrations/status.php
@@ -1,28 +1,3 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-/**
- * Displays the current status of migrations in all groups
- *
- * This task takes no config options
- *
- * @author Matt Button <matthew@sigswitch.com>
- */
-class Minion_Task_Migrations_Status extends Minion_Task {
-
-	/**
-	 * Execute the task
-	 *
-	 * @param array Config for the task
-	 */
-	public function execute(array $config)
-	{
-		$db        = Database::instance();
-		$model     = new Model_Minion_Migration($db);
-
-		$view = new View('minion/task/migrations/status');
-
-		$view->groups = $model->get_group_statuses();
-
-		echo $view;
-	}
-}
+class Minion_Task_Migrations_Status extends Kohana_Minion_Task_Migrations_Status { }
